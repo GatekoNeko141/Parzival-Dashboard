ddb9883f24316da21e4b87a0f884cadc203f5493
GatekoNeko141 <jhon1995jera@gmail.com>
2023-02-14 21:41:00 +0000

    Maquetacion del Dashboard

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import style from '../assets/css/Layout.module.css'
 
 const Layout = ({children}) => {
-  let LayoutHTML = (<>
-    <div className={`${style.contentDashboard}`}>
-      <h1>Layout</h1>
-      {children}
+  const [barExpanded] = useState(true)
+
+  const LayoutHTML = (<>
+    <div className={`no-scroll d-flex w-100 vh-100`}>
+      <div className={`${style.sideBar} ${(barExpanded ? style.sideBarExpanded : '')}`}>
+        <div className={`${style.logoContainer}`}></div>
+        <div className={`${style.navContainer} maq`}></div>
+      </div>
+      <div className={`${style.dashboardContainer}`}>
+        <div className={`${style.headerContainer} maq`}></div>
+        <div className={`${style.contentDynamic} maq`}>
+          {children}
+        </div>
+      </div>
     </div>
   </>)
 
